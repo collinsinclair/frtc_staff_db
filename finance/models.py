6ce99873eec1af8257d2e8d7bdb0180cf642edbc
Collin Sinclair <collin@collinsinclair.com>
2024-06-11 10:56:38 -0600

    add finance app and models

---
+++ finance/models.py
@@ -0,0 +1,34 @@
+import datetime
+
+from django.db import models
+
+from core.models import StaffMember
+
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+
+class ItemizedExpense(models.Model):
+    vendor = models.CharField(max_length=100)
+    reason = models.CharField(max_length=100)
+    date_of_transaction = models.DateField()
+    category = models.ForeignKey(
+        ExpenseCategory, on_delete=models.PROTECT, null=True, blank=True
+    )
+    value = models.DecimalField(decimal_places=2, max_digits=10)
+    note = models.TextField(null=True, blank=True)
+    receipt = models.ImageField()
+    purchaser = models.ForeignKey(StaffMember, on_delete=models.PROTECT)
+
+
+class ExpenseStatus(models.Model):
+    status = models.CharField(max_length=32)
+
+
+class ExpenseApproval(models.Model):
+    expense = models.ForeignKey(ItemizedExpense, on_delete=models.PROTECT)
+    status = models.ForeignKey(ExpenseStatus, on_delete=models.PROTECT)
+    reviewed_by = models.ForeignKey(StaffMember, on_delete=models.PROTECT)
+    review_date = models.DateField(default=datetime.date.today)
