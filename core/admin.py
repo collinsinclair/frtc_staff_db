45638383200d5a004cc83b3c267a42ed2dd2227e
Collin Sinclair <collin@collinsinclair.com>
2024-06-10 23:29:50 -0600

    initial commit

---
+++ core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+models = [
+    Location,
+    ProductionType,
+    Responsibility,
+    Role,
+    PhoneNumber,
+    Email,
+    StaffMember,
+    Production,
+]
+for model in models:
+    admin.site.register(model)
---
+++ core/models.py
@@ -0,0 +1,128 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductionType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Responsibility(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Responsibilities"
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    responsibilities = models.ManyToManyField(
+        Responsibility,
+        blank=True,
+        help_text="Choose any and only responsibilities that are inseparable from this role.",
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class PhoneNumber(models.Model):
+    phone_number = models.IntegerField(unique=True)
+    note = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        help_text="e.g. 'Collin Sinclair Cell' or 'PPA Main'",
+    )
+
+    def __str__(self):
+        return f"{self.phone_number}"
+
+
+class Email(models.Model):
+    email = models.EmailField(unique=True)
+    note = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Person(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(
+        max_length=32, blank=True, help_text="Leave blank if User is selected above."
+    )
+    last_name = models.CharField(
+        max_length=32, blank=True, help_text="Leave blank if User is selected above."
+    )
+    primary_phone_number = models.ForeignKey(
+        PhoneNumber,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="person_primary_phone",
+    )
+    other_phone_numbers = models.ManyToManyField(
+        PhoneNumber, blank=True, related_name="person_other_phones"
+    )
+    primary_email_address = models.ForeignKey(
+        Email,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="person_primary_email",
+    )
+    other_email_addresses = models.ManyToManyField(
+        Email, blank=True, related_name="person_other_emails"
+    )
+
+    def save(self, *args, **kwargs):
+        if self.user:
+            if not self.first_name:
+                self.first_name = self.user.first_name
+            if not self.last_name:
+                self.last_name = self.user.last_name
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        abstract = True
+
+
+class StaffMember(Person):
+    roles = models.ManyToManyField(Role, blank=True)
+    responsibilities = models.ManyToManyField(Responsibility, blank=True)
+
+
+class Production(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField(help_text="The date of the first rehearsal, usually.")
+    opening_date = models.DateField(
+        help_text="The date of the first performance, usually."
+    )
+    closing_date = models.DateField(
+        null=True, blank=True, help_text="The date of the last performance, usually."
+    )
+    staff = models.ManyToManyField(StaffMember, blank=True)
+    locations = models.ManyToManyField(Location, blank=True)
+    classification = models.ForeignKey(ProductionType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
